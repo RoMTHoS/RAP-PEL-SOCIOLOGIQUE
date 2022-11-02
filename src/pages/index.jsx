05737577e79456a7cd1e855ca1826a2a66b2b71e
RoMTHoS <matheosromain@gmail.com>
2022-11-02 10:03:28 +0100

    Delete projects file

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -111,13 +111,10 @@ function Resume() {
   let resume = [
     {
       company: 'Noemie',
-      title: 'CEO',
+      title: 'Etudiante Licence Socio',
       logo: logoPlanetaria,
       start: '2019',
-      end: {
-        label: 'Present',
-        dateTime: new Date().getFullYear(),
-      },
+      end: '2022',
     },
     {
       company: 'Bob',
@@ -146,7 +143,7 @@ function Resume() {
     <div className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
         <BriefcaseIcon className="h-6 w-6 flex-none" />
-        <span className="ml-3">Work</span>
+        <span className="ml-3">Créateur</span>
       </h2>
       <ol className="mt-6 space-y-4">
         {resume.map((role, roleIndex) => (
