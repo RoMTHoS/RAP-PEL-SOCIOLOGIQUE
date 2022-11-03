f1e298e8db3f6aee76a89551ca59234b6b57828a
RoMTHoS <matheosromain@gmail.com>
2022-11-03 11:32:53 +0100

    Correcting Dark mode

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -7,7 +7,7 @@ import { formatDate } from '@/lib/formatDate'
 
 function Article({ article }) {
   return (
-    <article className="text-justify md:grid md:grid-cols-4 md:items-baseline">
+    <article className="text-justify dark:text-zinc-200 md:grid md:grid-cols-4 md:items-baseline">
       <Card className="md:col-span-3">
         <Card.Title href={`/articles/${article.slug}`}>
           {article.title} - {article.stars}
@@ -18,8 +18,9 @@ function Article({ article }) {
           className="md:hidden"
           decorate
         >
-          {formatDate(article.date)}
           <Card.Image image={article.image1}></Card.Image>
+          <Card.Image image={article.image2}></Card.Image>
+          {formatDate(article.date)}
         </Card.Eyebrow>
         <Card.Description>{article.description}</Card.Description>
         <Card.Cta>Read article</Card.Cta>
@@ -49,7 +50,7 @@ export default function ArticlesIndex({ articles }) {
         title="Fiches RAP'PEL"
         intro="Chaque fiche analyse une punchline d'un rappeur et explique des notions dociologique en rapport."
       >
-        <div className="rounded-lg bg-white p-4 hover:bg-zinc-100 md:border-l md:border-zinc-300 md:pl-6 md:dark:border-zinc-700/40">
+        <div className="rounded-lg bg-slate-200 p-4 hover:bg-zinc-100 dark:bg-zinc-800 md:border-l md:border-zinc-300 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
             {articles.map((article) => (
               <Article key={article.slug} article={article} />
